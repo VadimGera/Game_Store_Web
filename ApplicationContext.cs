ffb97c2ab8daed77dacf5f0ae02a141cecf18c2f
Вадим Гермашев <124063584+VadimGera@users.noreply.github.com>
2024-07-08 18:13:12 +0300

    changed AppContext

--- ApplicationContext.cs
+++ ApplicationContext.cs
@@ -4,12 +4,20 @@ namespace GameStore
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> Users => Set<User>();
-        public ApplicationContext() => Database.EnsureCreated();
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options) 
+        {
+            Database.EnsureCreated();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=gamestore");
+            if (!optionsBuilder.IsConfigured) 
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=gamestore");
+            }
         }
     }
 }
