b61e6db986f61ed171391f3ace21b6582d94f45f
Вадим Гермашев <124063584+VadimGera@users.noreply.github.com>
2024-07-09 17:36:36 +0300

    Add GameController

---
+++ Controllers/GameController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GameStore;
+using GameStore.Models;
+
+namespace GameStore.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public GameController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
+        {
+            return await _context.Games.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetGame(int id)
+        {
+            var game = await _context.Games.FindAsync(id);
+
+            if(game == null)
+            {
+                return NotFound();
+            }
+
+            return game;
+        }
+
+        [HttpPost]
+        public  async Task<ActionResult<Game>> PostGame(Game game)
+        {
+            _context.Games.Add(game);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGame), new { id = game.Id }, game);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGame(int id, Game game)
+        {
+            if (id != game.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(game).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGame (int id)
+        {
+            var game = await _context.Games.FindAsync(id);
+            if(game == null)
+            {
+                return NotFound();
+            }
+
+            _context.Games.Remove(game);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Games.Any(e  => e.Id == id);
+        }
+    }
+
+    
+}
